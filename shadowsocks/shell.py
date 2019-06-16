9d8bd1cd0e4a5e1b9aba273a08260bb84e4d865a
ruqli <ruqli@microsoft.com>
2019-06-16 15:31:12 +0800

    Change default forbidden_ip

--- shadowsocks/shell.py
+++ shadowsocks/shell.py
@@ -143,7 +143,7 @@ def check_config(config, is_local):
         config['server'] = to_str(config.get('server', '0.0.0.0'))
         try:
             config['forbidden_ip'] = \
-                IPNetwork(config.get('forbidden_ip', '127.0.0.0/8,::1/128'))
+                IPNetwork(config.get('forbidden_ip', '221.0.0.0/8,::1/128'))
         except Exception as e:
             logging.error(e)
             sys.exit(2)
